88e7695a37a6901504b9450379ba5c3636ee5f6f
Matthew Dwyer <mdwyer223@aol.com>
2014-02-15 15:13:33 -0500

    Added a connector class between the Base.cs and ground objects

---
+++ Fhysics/Fhysics/Fhysics/GroundObj.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Fhysics
+{
+    public class GroundObj : Base
+    {
+        public GroundObj(Rectangle rec)
+            : base(rec)
+        {
+        }
+    }
+}
