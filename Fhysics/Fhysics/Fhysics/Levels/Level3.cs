ec436b945579b23e0ae5aabd4f14f123cf2887d9
Matthew Dwyer <mdwyer223@aol.com>
2014-02-16 03:00:23 -0500

    Next levels

---
+++ Fhysics/Fhysics/Fhysics/Levels/Level3.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Fhysics
+{
+    public class Level3 : MData
+    {
+        public Level3(Player p)
+            : base(p)
+        {
+            Directions d = Directions.NONE;
+            p.Position = new Vector2(5, 230);
+            bool push = true;
+
+            addObject(new Gap(new Rectangle(550, 0, 21, Game1.DisplayHeight)));
+
+            Vector2 pos = new Vector2(400, 0);
+            for (int i = 0; i < 24; i++)
+            {
+                Box b = new Box(pos, d, push);
+                addObject(b);
+                if(i == 15)
+                {
+                    d = Directions.ALL;
+                }
+                else
+                    d = Directions.NONE;
+
+                pos.Y += 25;
+            }
+
+            addObject(new Goal(new Rectangle(700, 230, 20, 20)));
+            messageText = "Gaps will end the game! Cover them!";
+        }
+
+        public override void Update(GameTime gameTime, Map map)
+        {
+            if (passed)
+            {
+                map.changeLevel(new Level4(map.Player));
+            }
+            base.Update(gameTime, map);
+        }
+
+        public override void resetMap(Map map)
+        {
+            map.changeLevel(new Level3(map.Player));
+        }
+    }
+}
---
+++ Fhysics/Fhysics/Fhysics/Levels/Level4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Fhysics
+{
+    public class Level4 : MData
+    {
+        public Level4(Player p)
+            : base(p)
+        {
+            p.Position = new Vector2(30, 450);
+            
+
+        }
+
+        public override void Update(GameTime gameTime, Map map)
+        {
+            base.Update(gameTime, map);
+        }
+
+        public override void resetMap(Map map)
+        {
+            map.changeLevel(new Level4(map.Player));
+        }
+    }
+}
